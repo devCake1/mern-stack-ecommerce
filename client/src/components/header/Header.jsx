0a90f280d60c19cb41fe69284df8482d9026f676
devCake1 <shipc4836@gmail.com>
2025-02-05 03:43:39 +0600

    add mini-cart to Header component

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -2,18 +2,29 @@ import "./Header.css";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping, faCircleUser, faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import MiniCart from "../miniCart/MiniCart";
 
 const Header = () => {
+  const showSidebar = () => {
+    document.getElementById("Header-sidebar").classList.replace("Header-close-sidebar", "Header-show-sidebar");
+    closeMiniCartSmall();
+  };
+
   const closeSidebar = () => {
     document.getElementById("Header-sidebar").classList.replace("Header-show-sidebar", "Header-close-sidebar");
   };
 
-  const showSidebar = () => {
-    document.getElementById("Header-sidebar").classList.replace("Header-close-sidebar", "Header-show-sidebar");
+  const showMiniCartSmall = () => {
+    document.getElementById("Header-mini-cart-small").classList.replace("Header-close-mini-cart-small", "Header-show-mini-cart-small");
+    closeSidebar();
+  };
+
+  const closeMiniCartSmall = () => {
+    document.getElementById("Header-mini-cart-small").classList.replace("Header-show-mini-cart-small", "Header-close-mini-cart-small");
   };
 
   return (
-    <div className="Header bg-blue-200">
+    <div className="Header bg-blue-200 relative">
       <div className="Header-logo bg-blue-300 text-center text-xl font-bold">
         Lorem Ipsum
       </div>
@@ -25,25 +36,28 @@ const Header = () => {
       </div>
       <div className="Header-navbar">
         <div className="Header-nav-link-div relative">
-          <Link to="" className="Header-nav-link">Home</Link>
+          <Link to="" className="Header-nav-link hover:bg-blue-300">Home</Link>
         </div>
-        <div className="Header-nav-link-div relative">
+        <div className="Header-nav-link-div hover:bg-blue-300">
           <Link to="" className="Header-nav-link">Shop</Link>
         </div>
-        <div className="Header-nav-link-div relative">
+        <div className="Header-nav-link-div hover:bg-blue-300">
           <Link to="" className="Header-nav-link">Reviews</Link>
         </div>
-        <div className="Header-nav-link-div relative">
+        <div className="Header-nav-link-div Header-dropdown-div hover:bg-blue-300 relative">
           <Link to="" className="Header-nav-link"><FontAwesomeIcon icon={faCartShopping}/></Link>
+          <div className="Header-dropdown-content absolute top-full right-0">
+            <MiniCart/>
+          </div>
         </div>
-        <div className="Header-nav-link-div Header-dropdown-div relative">
+        <div className="Header-nav-link-div Header-dropdown-div hover:bg-blue-300 relative">
           <Link to="" className="Header-nav-link"><FontAwesomeIcon icon={faCircleUser}/></Link>
           <div className="Header-dropdown-content absolute top-full right-0 bg-blue-200">
             <div className="Header-dropdown-link-div">
-              <Link to="" className="Header-dropdown-link">Sign In</Link>
+              <Link to="" className="Header-dropdown-link hover:bg-blue-300">Sign In</Link>
             </div>
             <div className="Header-dropdown-link-div">
-              <Link to="" className="Header-dropdown-link">Sign Up</Link>
+              <Link to="" className="Header-dropdown-link hover:bg-blue-300">Sign Up</Link>
             </div>
           </div>
         </div>
@@ -65,7 +79,7 @@ const Header = () => {
           <Link to="" className="Header-sidebar-nav-link">Reviews</Link>
         </div>
         <div>
-          <Link to="" className="Header-sidebar-nav-link">
+          <Link to="" className="Header-sidebar-nav-link" onClick={showMiniCartSmall}>
             <FontAwesomeIcon icon={faCartShopping}/>
           </Link>
         </div>
@@ -82,6 +96,14 @@ const Header = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="Header-mini-cart-small Header-close-mini-cart-small" id="Header-mini-cart-small">
+        <div className="relative">
+          <button className="Header-mini-cart-small-close-button absolute top-0 right-0 bg-gray-400 text-white cursor-pointer" onClick={closeMiniCartSmall}>
+            <FontAwesomeIcon icon={faXmark}/>
+          </button>
+        </div>
+        <MiniCart/>
       </div>
     </div>
   );
---
+++ client/src/components/miniCart/MiniCart.jsx
@@ -0,0 +1,14 @@
+import "./MiniCart.css";
+
+const MiniCart = () => {
+  return (
+    <div className="MiniCart shadow-xl shadow-gray-600/50">
+      <div className="MiniCart-empty">
+        <h3 className="text-center">Shopping cart is empty</h3>
+        <button className="bg-blue-300 cursor-pointer">Shop Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default MiniCart;
