56e5d8c96296e27142638f852a7a8279dfc17718
devCake1 <shipc4836@gmail.com>
2025-02-06 04:28:13 +0600

    add categories to Home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,14 +1,39 @@
 import "./Home.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/categories?limit=6")
+    .then((res) => {
+      setCategories(res.data.categories);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }, []);
+
   return (
     <div>
-      <div className="Home-top-banner relative text-center">
+      {/* ----- top banner ----- */}
+      <div className="Home-top-banner margin-bottom-fifty-px relative text-center">
         <div className="Home-top-banner-content text-white">
           <h3 className="text-3xl font-bold">Lorem Ipsum</h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, esse! Commodi fuga unde earum temporibus eos.</p>
-          <button className="bg-blue-300 text-black cursor-pointer">Shop Now</button>
+          <button className="button-style bg-blue-300 text-black cursor-pointer">Shop Now</button>
         </div>
+      </div>
+
+      {/* ----- categories ----- */}
+      <h3 className="margin-bottom-ten-px text-3xl font-bold text-center">Categories</h3>
+      <div className="Home-categories grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 text-center">
+        {categories.map((category) => <div key={category._id}><Link className="Home-categories-link bg-blue-200">{category.category}</Link></div>)}
+      </div>
+      <div className="text-center margin-bottom-fifty-px margin-top-ten-px">
+        <button className="button-style bg-black text-white cursor-pointer">View All</button>
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const categoryRoutes = require("./routes/categoryRoutes");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const categoryRoutes = require("./routes/categoryRoutes");
 const app = express();
 const port = 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to MERN-STACK-ECOMMERCE");
